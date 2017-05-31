6b9710c812813cec0786630650820aac344bdabe
Michael Prather <mprather1@gmail.com>
2017-05-31 16:05:55 +0000

    http-proxy-middleware

--- src/server.js
+++ src/server.js
@@ -2,30 +2,52 @@ import fs from 'fs'
 import os from 'os'
 import path from 'path'
 import logger from 'winston-color'
-import httpProxy from 'http-proxy'
+import connect from 'connect'
+import https from 'https'
+import pkg from '../package.json'
+import chalk from 'chalk'
+import httpProxy from 'http-proxy-middleware'
 
 const domainName = process.env.DOMAIN_NAME
-
+const packageName = pkg.name
+const packageVersion = pkg.version
 const homedir = os.homedir()
+const port = process.env.PORT
 const sslPath = path.join(homedir, 'letsencrypt', 'live', domainName)
 
-const proxy = httpProxy.createServer({
+var options = {
   target: {
     host: 'localhost',
     port: 8000
   },
-  ssl: {
-    key: fs.readFileSync(sslPath + '/privkey.pem'),
-    cert: fs.readFileSync(sslPath + '/fullchain.pem')
-  }
+  logProvider: logProvider
+}
+
+var serverConfig = {
+  key: fs.readFileSync(sslPath + '/privkey.pem'),
+  cert: fs.readFileSync(sslPath + '/fullchain.pem')
+}
+
+var proxy = httpProxy(options)
+
+var app = connect()
+
+app.use(proxy)
+
+var server = https.createServer(serverConfig, app).listen(port)
+
+server.on('listening', function (req, res) {
+  logger.info(chalk.bgBlack.cyan(packageName), 'v.' + chalk.bgBlack.green(packageVersion), 'listening on port', chalk.bgBlack.yellow(port))
 })
 
-proxy.on('proxyReq', function (req, res) {
-  logger.info(req.method, 'http://' + req._headers.host + req.path)
+server.on('request', function (req, res) {
+  logger.info(chalk.cyan(req.method), 'http://' + req.headers.host + req.url, res.statusCode)
 })
 
-proxy.on('error', function (err) {
+server.on('error', function (err) {
   logger.error(err)
 })
 
-proxy.listen(443)
+function logProvider (provider) {
+  return logger
+}
